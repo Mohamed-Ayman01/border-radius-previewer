635e8afb5670bac0f42abbee3de86c154975dbd4
Mohamed-Ayman01 <lord39597@gmail.com>
2023-02-21 20:09:34 +0400

    made sliders feat. & randomized values feat.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Border-Radius-Previewer
+
 🟢 tool to help make CSS border shapes
+
+- **[live demo](https://mohamed-ayman01.github.io/Border-Radius-Previewer/)**
--- js/app.js
+++ js/app.js
@@ -1,52 +1,143 @@
-let previewBox = document.querySelector(".preview-box");
 let previewElement = document.querySelector(".preview-element");
-let topLeftPoint = document.querySelector(".point.top-left");
-let topRightPoint = document.querySelector(".point.top-right");
-let bottomRightPoint = document.querySelector(".point.bottom-right");
-let bottomLeftPoint = document.querySelector(".point.bottom-left");
-let outputTextArea = document.querySelector("textarea.css-output")
-
-let allPoints = document.querySelectorAll(".point");
+let outputTextArea = document.querySelector("textarea.css-output");
 let borderRadiusValue = ["0%", "0%", "0%", "0%"];
 
-// * Change Border Values 
-allPoints.forEach((el) => {
-  el.addEventListener("input", () => {
-    if (el === topLeftPoint) {
-      borderRadiusValue[0] = `${el.value}%`;
+// Make Random Values Appear On Load
+function generateRandomBorders() {
+  function getRandomNumInRange(start, range) {
+    let number = Math.floor(Math.random() * range + start);
+
+    while (number > range) {
+      number = Math.floor(Math.random() * range + start);
     }
-    if (el === topRightPoint) {
-      borderRadiusValue[1] = `${el.value}%`;
+
+    return number;
+  }
+
+  for (let i = 0; i < borderRadiusValue.length; i++) {
+    borderRadiusValue[i] = `${getRandomNumInRange(0, 100)}%`;
+  }
+
+  previewElement.style = `border-radius: ${borderRadiusValue.join(" ")}`;
+  outputTextArea.innerHTML = `border-radius: ${borderRadiusValue.join(" ")}`;
+
+  let allTracks = document.querySelectorAll(".track");
+  for (let i = 0; i < allTracks.length; i++) {
+    let point = allTracks[i].firstElementChild;
+
+    if (allTracks[i].classList.contains("vertical-track")) {
+      point.style.top = `${parseInt(borderRadiusValue[i]) - 6}%`;
+    } else {
+      point.style.left = `${parseInt(borderRadiusValue[i]) - 6}%`;
     }
-    if (el === bottomRightPoint) {
-      borderRadiusValue[2] = `${el.value}%`;
-    }
-    if (el === bottomLeftPoint) {
-      borderRadiusValue[3] = `${el.value}%`;
-    }
-    previewElement.style = `border-radius: ${borderRadiusValue.join(" ")}`;
-    outputTextArea.innerHTML = `border-radius: ${borderRadiusValue.join(" ")}`;
+  }
+}
+
+window.onload = generateRandomBorders;
+
+// Vertical Tracks Logic
+let verticalTracks = document.querySelectorAll(".vertical-track");
+function verticalTracksSlide(trackList) {
+  trackList.forEach((track) => {
+    let point = track.firstElementChild;
+    let isMouseDown;
+
+    track.addEventListener("mousedown", (e) => {
+      if (e.target === point) {
+        isMouseDown = true;
+      }
+    });
+
+    track.addEventListener("mouseup", () => {
+      isMouseDown = false;
+    });
+
+    track.addEventListener("mouseleave", () => {
+      isMouseDown = false;
+    });
+
+    track.addEventListener("mousemove", (e) => {
+      let axis = e.pageY - track.getBoundingClientRect().top - 12;
+      let axisPrecentage = Math.round((axis * 100) / 200);
+
+      if (isMouseDown === true && axis >= 0 && axis <= 200) {
+        point.style.top = `${axis}px`;
+
+        if (track.classList.contains("track-left")) {
+          borderRadiusValue[0] = `${axisPrecentage}%`;
+        } else {
+          borderRadiusValue[2] = `${axisPrecentage}%`;
+        }
+
+        previewElement.style = `border-radius: ${borderRadiusValue.join(" ")}`;
+        outputTextArea.innerHTML = `border-radius: ${borderRadiusValue.join(
+          " ",
+        )}`;
+      }
+    });
   });
-});
+}
+
+verticalTracksSlide(verticalTracks);
+
+// Horizental Tracks Logic
+let horizentalTracks = document.querySelectorAll(".horizental-track");
+function horizentalTracksSlide(trackList) {
+  trackList.forEach((track) => {
+    let point = track.firstElementChild;
+    let isMouseDown;
+
+    track.addEventListener("mousedown", (e) => {
+      if (e.target === point) {
+        isMouseDown = true;
+      }
+    });
+
+    track.addEventListener("mouseup", () => {
+      isMouseDown = false;
+    });
+
+    track.addEventListener("mouseleave", () => {
+      isMouseDown = false;
+    });
+
+    track.addEventListener("mousemove", (e) => {
+      let axis = e.pageX - track.getBoundingClientRect().left - 12;
+      let axisPrecentage = Math.round((axis * 100) / 200);
+
+      if (isMouseDown === true && axis >= 0 && axis <= 200) {
+        point.style.left = `${axis}px`;
+
+        if (track.classList.contains("track-top")) {
+          borderRadiusValue[1] = `${axisPrecentage}%`;
+        } else {
+          borderRadiusValue[3] = `${axisPrecentage}%`;
+        }
+
+        previewElement.style = `border-radius: ${borderRadiusValue.join(" ")}`;
+        outputTextArea.innerHTML = `border-radius: ${borderRadiusValue.join(
+          " ",
+        )}`;
+      }
+    });
+  });
+}
+
+horizentalTracksSlide(horizentalTracks);
 
 // Copy Feature
 let copyBtn = document.querySelector("button.copy");
+let copiedMsgBox = document.querySelector(".copied-msg");
 
 copyBtn.addEventListener("click", () => {
   outputTextArea.select();
   document.execCommand("copy");
+
+  copiedMsgBox.style.top = `1rem`;
+  setTimeout(() => (copiedMsgBox.style.top = "-100px"), 2e3);
 });
 
-// let topLeftDown;
-// topLeftPoint.addEventListener('mousedown', () => {
-//   topLeftDown = true;
-// });
-// topLeftPoint.addEventListener('mouseup', () => {
-//   topLeftDown = false;
-// });
-// previewBox.addEventListener('mousemove', (e) => {
-//   if (topLeftDown && (e.clientY - previewBox.getClientRects()[0].top) >= 0 && e.clientY - previewBox.getClientRects()[0].top < (300 - 20)) {
-//     topLeftPoint.style.top = `${e.clientY - previewBox.getClientRects()[0].top}px`
-//     // topLeftPoint.style.left = `${e.clientX - previewBox.getClientRects()[0].left - 10}px`
-//   }
-// });
+// Generate Random Border Values Onclick
+let randomizeBtn = document.querySelector("button.random");
+
+randomizeBtn.addEventListener("click", generateRandomBorders);
